4838956ddca9b708c57a60d27a9dfe0cca0096b7
digitribesthlm <patrik@makeablesthlm.se>
2024-11-19 20:05:26 +0100

    next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // Add this to handle any potential image domains
+  images: {
+    domains: [],
+  },
 }
 
 module.exports = nextConfig 
